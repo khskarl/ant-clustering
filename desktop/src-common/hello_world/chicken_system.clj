d47584e58f5f9dc86124737df40c6a1c2ddabd14
Eu <karllhenning64@gmail.com>
2017-03-20 23:40:38 -0300

    Started rewriting ant code into a separate file

--- desktop/src-common/hello_world/chicken_system.clj
+++ desktop/src-common/hello_world/chicken_system.clj
@@ -1,8 +1,77 @@
 (ns hello-world.chicken-system)
 
 
-(defn make-dead-map
+(def dimension 3)
+
+(def dead-grid (make-grid dimension))
+(def alive-grid (make-grid dimension))
+
+(defstruct ant :x :y)
+(def ants (create-ants 1))
+
+(defn get-tile
+  [grid [y x]]
+  (get-in grid [y x]))
+
+(defn make-grid
+  [dimension]
+  (apply vector 
+         (map (fn [_] 
+                (apply vector (map (fn [_] (ref false)) 
+                                   (range dimension)))) 
+              (range dimension))))
+
+(defn create-ant
+  [[i j]] 
+  (ref-set (get-tile alive-grid [i j]) true)
+  (ref (struct ant j i)))
+
+(defn create-ants
   ""
-  [height width]
-  (make-array Boolean/TYPE height width))
+  [num-ants]
+  (dosync (loop [num-ants-left num-ants
+                 new-ants []]
+            (if (zero? num-ants-left)
+              new-ants
+              (let [i (rand-int dimension)
+                    j (rand-int dimension)
+                    grid alive-grid
+                    is-tile-busy (deref (get-tile grid [i j]))]
+                (if is-tile-busy
+                  (recur num-ants-left new-ants)
+                  (recur (dec num-ants-left) (conj new-ants (create-ant [i j])))))))))
+
+(defn create-bodies
+  [num-bodies]
+  "I do nothing :D")
+
+(defn move-ant
+  ""
+  [ant [dx dy]]
+  (dosync
+   (let [x (:x ant)
+         y (:y ant)
+         new-x (+ x dx)
+         new-y (+ y dy)]
+     (ref-set (get-tile alive-grid [y x] false))
+     (ref-set (get-tile alive-grid [new-y new-x] true))
+     (ref-set ant (struct new-x new-y))
+     )
+   ))
+
+
+(defn dec-tile [x]
+  (let [new-x (dec x)]
+    (if (< new-x 0)
+      (- dimension 1)
+      new-x)))
+
+(defn inc-tile [x]
+  (let [new-x (inc x)]
+    (if (>= new-x dimension)
+      0
+      new-x)))
+
+(defn random-direction [] (rand-nth [:down :up :left :right]))
+
 
--- desktop/src-common/hello_world/core.clj
+++ desktop/src-common/hello_world/core.clj
@@ -4,11 +4,7 @@
             [play-clj.g2d :refer :all]))
 
 (def screen-height 800)
-(def num-tiles-per-side 50)
-(def tile-size (/ screen-height num-tiles-per-side))
-
-(def dead-map (make-array Boolean/TYPE num-tiles-per-side num-tiles-per-side))
-
+(def tile-size 25)
 (defn create-chicken
   "Creates a chicken entity for play-clj"
   [x y]
@@ -25,62 +21,28 @@
          :width tile-size :height tile-size
          :alive false))
 
-(defn create-random-chickens
-  "Create N random chickens in grid"
-  [num-chickens entities]
-  (into entities (repeatedly num-chickens #(create-chicken (Math/floor (rand screen-height))
-                                                           (Math/floor (rand screen-height))))))
-
-(defn create-random-dead
-  "Create N random dead chickens in grid"
-  [num-chickens entities]
-  (into entities (repeatedly num-chickens #(create-dead (Math/floor (rand screen-height))
-                                                        (Math/floor (rand screen-height))))))
-
-(defn dec-tile [x]
-  (let [new-x (- x tile-size)]
-    (if (< new-x 0)
-      (- screen-height tile-size)
-      new-x)))
-
-(defn inc-tile [x]
-  (let [new-x (+ x tile-size)]
-    (if (>= new-x screen-height)
-      0
-      new-x)))
-
-(defn random-direction [] (rand-nth [:down :up :left :right]))
-
-(defn move-chicken
-  ""
-  [direction entity]
-  (case direction
-    :down (update entity :y dec-tile)
-    :up   (update entity :y inc-tile)
-    :left (update entity :x dec-tile)
-    :right (update entity :x inc-tile)
-    nil))
-
-(defn update-alive-chickens 
-  [entities]
-  (map (fn [entity]
-         (->> entity
-              (move-chicken (random-direction))))
-       entities))
+;; (defn update-alive-chickens 
+;;   [entities]
+;;   (map (fn [entity]
+;;          (->> entity
+;;               (move-chicken (random-direction))))
+;;        entities))
 
 (defscreen main-screen
   :on-show
   (fn [screen entities]
     (update! screen :renderer (stage))
-    (->> entities
-         (create-random-chickens 50)))
+    ;; (->> entities
+    ;;      (create-random-chickens 50))
+    )
+  
   
   :on-render
   (fn [screen entities]
     (clear!)
     (Thread/sleep 50)
     (->> entities
-         (update-alive-chickens)
+         ;; (update-alive-chickens)
          (render! screen)))
 
   :on-key-down
